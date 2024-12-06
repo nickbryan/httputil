194b7cadc0af867906850a82212a0fbc3a402ddd
Nick Bryan <nickbryan.dev@gmail.com>
2024-12-06 08:59:47 +0000

    Revert Serve to listen in the goroutine as NotifyContext seems more idiomatic

--- options.go
+++ options.go
@@ -5,6 +5,7 @@ import (
 )
 
 type (
+	// ServerOption allows default config values to be overridden.
 	ServerOption func(so *serverOptions)
 
 	serverOptions struct {
@@ -16,36 +17,42 @@ type (
 	}
 )
 
+// WithIdleTimeout sets the idle timeout for the server.
 func WithIdleTimeout(timeout time.Duration) ServerOption {
 	return func(so *serverOptions) {
 		so.idleTimeout = timeout
 	}
 }
 
+// WithReadHeaderTimeout sets the timeout for reading the request header.
 func WithReadHeaderTimeout(timeout time.Duration) ServerOption {
 	return func(so *serverOptions) {
 		so.readHeaderTimeout = timeout
 	}
 }
 
+// WithReadTimeout sets the timeout for reading the request body.
 func WithReadTimeout(timeout time.Duration) ServerOption {
 	return func(so *serverOptions) {
 		so.readTimeout = timeout
 	}
 }
 
+// WithShutdownTimeout sets the timeout for gracefully shutting down the server.
 func WithShutdownTimeout(timeout time.Duration) ServerOption {
 	return func(so *serverOptions) {
 		so.shutdownTimeout = timeout
 	}
 }
 
+// WithWriteTimeout sets the timeout for writing the response.
 func WithWriteTimeout(timeout time.Duration) ServerOption {
 	return func(so *serverOptions) {
 		so.writeTimeout = timeout
 	}
 }
 
+// mapServerOptionsToDefaults applies the provided ServerOptions to a default serverOptions struct.
 func mapServerOptionsToDefaults(opts []ServerOption) serverOptions {
 	const (
 		defaultShutdownTimeout = 5 * time.Second
--- server.go
+++ server.go
@@ -1,29 +1,19 @@
+// Package httputil provides utilities for working with HTTP servers and clients.
 package httputil
 
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log/slog"
 	"net/http"
-	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 )
 
-var ErrSignalChannelClosed = errors.New("signal channel has been closed unexpectedly")
-
-type SignalInterruptError struct {
-	Signal os.Signal
-}
-
-func (e *SignalInterruptError) Error() string {
-	return fmt.Sprintf("received interrupt signal: %s", e.Signal)
-}
-
+// Server is an HTTP server with graceful shutdown capabilities.
 type Server struct {
+	// Listener is implemented by a *http.Server, the interface allows us to test Serve.
 	Listener interface {
 		ListenAndServe() error
 		Shutdown(ctx context.Context) error
@@ -36,69 +26,63 @@ type Server struct {
 	shutdownTimeout time.Duration
 }
 
+// NewServer creates a new Server instance with the specified logger, address, and options.
+// The options parameter allows for customization of server settings such as timeouts.
 func NewServer(logger *slog.Logger, address string, options ...ServerOption) *Server {
 	opts := mapServerOptionsToDefaults(options)
 
 	server := &Server{
-		Listener:        nil,
+		Listener:        nil, // We need to set Listener after we have server as we pass that to Handler.
 		logger:          logger,
 		router:          http.NewServeMux(),
 		shutdownTimeout: opts.shutdownTimeout,
 		address:         address,
 	}
 
-	// TODO: leave like this or don't be exhaustive?
 	server.Listener = &http.Server{
-		Addr:                         server.address,
-		Handler:                      server,
-		DisableGeneralOptionsHandler: false,
-		TLSConfig:                    nil,
-		ReadTimeout:                  opts.readTimeout,
-		ReadHeaderTimeout:            opts.readHeaderTimeout,
-		WriteTimeout:                 opts.writeTimeout,
-		IdleTimeout:                  opts.idleTimeout,
-		MaxHeaderBytes:               http.DefaultMaxHeaderBytes,
-		TLSNextProto:                 nil,
-		ConnState:                    nil,
-		// TODO: use this or clone the writeHandler so we can provide a static message and add the error as an attribute? Would also allow us to set pc?
-		ErrorLog:    slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		BaseContext: nil,
-		ConnContext: nil,
+		Addr:              server.address,
+		Handler:           server, // Server is the handler due to ServeHTTP. This allows us to test the server.
+		ReadTimeout:       opts.readTimeout,
+		ReadHeaderTimeout: opts.readHeaderTimeout,
+		WriteTimeout:      opts.writeTimeout,
+		IdleTimeout:       opts.idleTimeout,
+		MaxHeaderBytes:    http.DefaultMaxHeaderBytes,
+		ErrorLog:          slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		// TODO: use this or clone the writeHandler so we can provide a static message
+		// and add the error as an attribute? Would also allow us to set pc?
 	}
 
 	return server
 }
 
+// ServeHTTP delegates the request handling to the underlying router.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
+// Serve starts the HTTP server and listens for incoming requests.
+// It gracefully shuts down the server when it receives a SIGINT, SIGTERM, or SIGQUIT signal.
 func (s *Server) Serve(ctx context.Context) {
-	awaitConnsClosed := make(chan any)
+	awaitSignalCtx, cancelSignalCtx := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
-		awaitSignal := make(chan os.Signal, 1)
-		signal.Notify(awaitSignal, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		defer cancelSignalCtx()
 
-		s.logger.InfoContext(ctx, "Server started", slog.String("address", s.address))
-		sig := <-awaitSignal
-		// TODO: select and handle ctx.Done?
-
-		s.logger.InfoContext(ctx, "Server shutting down due to signal interrupt", slog.String("signal", sig.String()))
-
-		shutdownCtx, cancel := context.WithTimeout(ctx, s.shutdownTimeout)
-		defer cancel()
-
-		if err := s.Listener.Shutdown(shutdownCtx); err != nil {
-			s.logger.ErrorContext(ctx, "Server failed to shutdown gracefully", slog.String("error", err.Error()))
+		// When Listener.Shutdown is called http.ErrServerClosed is returned immediately unblocking this
+		// goroutine. Shutdown then blocks while it handles graceful shutdown.
+		if err := s.Listener.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			s.logger.ErrorContext(ctx, "Server failed to listen and serve", slog.String("error", err.Error()))
 		}
-
-		close(awaitConnsClosed)
 	}()
 
-	if err := s.Listener.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		s.logger.ErrorContext(ctx, "Server failed to listen and serve", slog.String("error", err.Error()))
+	s.logger.InfoContext(ctx, "Server started", slog.String("address", s.address))
+	<-awaitSignalCtx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(ctx, s.shutdownTimeout)
+	defer cancel()
+
+	if err := s.Listener.Shutdown(shutdownCtx); err != nil {
+		s.logger.ErrorContext(ctx, "Server failed to shutdown gracefully", slog.String("error", err.Error()))
 	}
 
-	<-awaitConnsClosed
-	s.logger.InfoContext(ctx, "Server shutdown completed")
+	s.logger.InfoContext(ctx, "Server shutdown")
 }
