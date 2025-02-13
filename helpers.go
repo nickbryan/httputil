aef1bc5114f39b455a244fbdbea8716c4914a6fa
Nick Bryan <nickbryan.dev@gmail.com>
2025-02-13 15:58:53 +0000

    Try out reflection api

--- helpers.go
+++ helpers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strconv"
 	"time"
 
@@ -160,4 +161,81 @@ func (b *ParamBuilder[T]) Resolve() (T, error) {
 	}
 
 	return result, nil
+}
+
+func Params(r *http.Request, output any) error {
+	outputVal := reflect.ValueOf(output).Elem() // Get the addressable value of the output struct
+	if outputVal.Kind() != reflect.Struct {
+		return fmt.Errorf("output must be a pointer to a struct, got %T", output)
+	}
+
+	for i := 0; i < outputVal.NumField(); i++ {
+		field := outputVal.Type().Field(i)
+
+		queryTag := field.Tag.Get("query")
+		headerTag := field.Tag.Get("header")
+		pathTag := field.Tag.Get("path")
+
+		if queryTag == "" && headerTag == "" && pathTag == "" {
+			continue
+		}
+
+		var paramName, paramValue string
+
+		if queryTag != "" {
+			paramName = queryTag
+			paramValue = r.URL.Query().Get(paramName)
+		} else if headerTag != "" {
+			paramName = headerTag
+			paramValue = r.Header.Get(paramName)
+		} else if pathTag != "" {
+			paramName = pathTag
+			paramValue = r.PathValue(paramName)
+		}
+
+		if paramValue == "" {
+			continue
+		}
+
+		fieldVal := outputVal.Field(i) // Get the value of the current field
+
+		switch fieldVal.Interface().(type) {
+		case string:
+			fieldVal.SetString(paramValue)
+		case int:
+			v, err := strconv.Atoi(paramValue)
+			if err != nil {
+				return fmt.Errorf("failed to convert %s to int: %w", paramName, err)
+			}
+			fieldVal.SetInt(int64(v))
+		case bool:
+			v, err := strconv.ParseBool(paramValue)
+			if err != nil {
+				return fmt.Errorf("failed to convert %s to bool: %w", paramName, err)
+			}
+			fieldVal.SetBool(v)
+		case float64:
+			v, err := strconv.ParseFloat(paramValue, 64)
+			if err != nil {
+				return fmt.Errorf("failed to convert %s to float64: %w", paramName, err)
+			}
+			fieldVal.SetFloat(v)
+		case time.Time:
+			v, err := time.Parse(time.RFC3339, paramValue)
+			if err != nil {
+				return fmt.Errorf("failed to convert %s to time.Time: %w", paramName, err)
+			}
+			fieldVal.Set(reflect.ValueOf(v))
+		case uuid.UUID:
+			v, err := uuid.Parse(paramValue)
+			if err != nil {
+				return fmt.Errorf("failed to convert %s to uuid.UUID: %w", paramName, err)
+			}
+			fieldVal.Set(reflect.ValueOf(v))
+		default:
+			return fmt.Errorf("unsupported field type: %T", fieldVal.Interface())
+		}
+	}
+
+	return nil
 }
